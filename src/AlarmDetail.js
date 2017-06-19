b3bfc317f0b96fdadd3b1ab23411be6df69ef7e8
khoadoan91 <khoadnd91@gmail.com>
2017-06-18 18:30:48 -0700

    Add snooze feature

--- src/AlarmDetail.js
+++ src/AlarmDetail.js
@@ -2,17 +2,19 @@ import React from "react";
 import Sound from "react-sound";
 
 export default class extends React.Component {
-  state = { enable: true, isRing: false };
+  state = { enable: true, isRing: false, isSnooze: false, countdown: 0 };
 
   componentDidMount() {
-    this.enable();
+    this.enable(this.props.alarm);
+    this.snoozeAlarm = null;
+    this.countdown = 0;
   }
 
   handleChange = () => {
     if (this.state.enable) {
       this.disable();
     } else {
-      this.enable();
+      this.enable(this.props.alarm);
     }
     this.setState({ enable: !this.state.enable });
   };
@@ -21,9 +23,9 @@ export default class extends React.Component {
     clearTimeout(this.timeoutId);
   };
 
-  enable = () => {
+  enable = alarm => {
     const now = new Date();
-    const { alarm } = this.props;
+    // const { alarm } = this.props;
     const alarmInSecond = alarm.getHours() * 3600 + alarm.getMinutes() * 60;
     let countdown =
       alarmInSecond -
@@ -33,6 +35,7 @@ export default class extends React.Component {
       countdown += 24 * 60 * 60;
     }
     console.log("countdown", countdown);
+    this.setState({ countdown });
     countdown *= 1000;
 
     this.timeoutId = setTimeout(() => {
@@ -57,7 +60,7 @@ export default class extends React.Component {
         <button onClick={() => this.props.onRemove(this.props.order)}>
           Delete
         </button>
-        {this.state.isRing ? this.renderSound() : null}
+        {this.state.isRing || this.state.isSnooze ? this.renderSound() : null}
       </li>
     );
   }
@@ -65,20 +68,47 @@ export default class extends React.Component {
   renderSound = () => {
     return (
       <div>
-        <Sound
-          url="../sounds/Fire_pager-jason.mp3"
-          playStatus={Sound.status.PLAYING}
-          playingFromPosition={0}
-        />
+        {this.state.isRing
+          ? <Sound
+              url="../sounds/Fire_pager-jason.mp3"
+              playStatus={Sound.status.PLAYING}
+              playingFromPosition={0}
+            />
+          : null}
+        {this.state.isSnooze && !this.state.isRing
+          ? this.renderCountdown()
+          : <button
+              onClick={() => {
+                if (!this.snoozeAlarm) {
+                  this.snoozeAlarm = new Date(this.props.alarm.getTime());
+                }
+                this.snoozeAlarm.setMinutes(this.snoozeAlarm.getMinutes() + 1);
+                this.enable(this.snoozeAlarm);
+                this.setState({ isSnooze: true, isRing: false });
+                clearInterval(this.interval);
+                this.interval = setInterval(() => {
+                  this.setState({ countdown: this.state.countdown - 1 });
+                }, 1000);
+              }}
+            >
+              Snooze
+            </button>}
+
         <button
           onClick={() => {
-            this.enable();
-            this.setState({ isRing: false });
+            this.enable(this.props.alarm);
+            this.snoozeAlarm = null;
+            this.setState({ isSnooze: false, isRing: false });
+            clearInterval(this.interval);
           }}
         >
           Dismiss
         </button>
       </div>
     );
+  };
+
+  renderCountdown = () => {
+    return <div>Will ring on {this.state.countdown} seconds</div>;
   };
 }
